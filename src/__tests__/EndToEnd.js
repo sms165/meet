de5a4d63fd69f31d2085dd64531f92f95ada62b3
sms165 <61697688+sms165@users.noreply.github.com>
2022-07-11 22:59:07 +0200

    set up Atatus

--- src/__tests__/EndToEnd.js
+++ src/__tests__/EndToEnd.js
@@ -1,38 +1,38 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
-describe('show/hide an event details', () => {
+describe("show/hide an event details", () => {
   let browser;
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
     browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 250, // slow down by 250ms
-       ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
-      });
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+      ignoreDefaultArgs: ["--disable-extensions"], // ignores default setting that causes timeout errors
+    });
     page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
   });
 
-  afterAll(() => {
-    browser.close();
+  afterAll(async () => {
+    await browser.close();
   });
 
-  test('An event element is collapsed by default', async () => {
-    const eventDetails = await page.$('.event .additional-details');
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .additional-details");
     expect(eventDetails).toBeNull();
   });
 
-  test('User can expand an event to see its details', async () => {
-    await page.click('.event .btn-details');
-    const eventDetails = await page.$('.event .additional-details');
+  test("User can expand an event to see its details", async () => {
+    await page.click(".event .btn-details");
+    const eventDetails = await page.$(".event .additional-details");
     expect(eventDetails).toBeDefined();
   });
 
-  test('User can collapse an event to hide its details', async () => {
-    await page.click('.event .btn-details');
-    const eventDetails = await page.$('.event .additional-details');
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".event .btn-details");
+    const eventDetails = await page.$(".event .additional-details");
     expect(eventDetails).toBeNull();
   });
 });
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import * as atatus from 'atatus-spa';
+atatus.config('a812f9badf794339ab40625bd5fdc74b').install();
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -25,3 +26,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
