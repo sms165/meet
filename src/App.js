220598c30099d91f42a9b615fa9293f214898343
sms165 <61697688+sms165@users.noreply.github.com>
2022-07-16 11:16:28 +0200

    added a welcome screen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from "./api";
+import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
 import "./nprogress.css";
 import { WarningAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
   state = {
@@ -13,14 +14,25 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     currentLocation: "all",
+    showWelcomeScreen: undefined,
   };
 
-  componentDidMount() {
-    getEvents().then((events) => {
-      this.setState({ events, locations: extractLocations(events) });
-    });
+  async componentDidMount() {
+    this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+  true;
+    const searchParams = new URLSearchParams(window.location.search);
+  const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+  }); }
   }
-
+  
   componentWillUnmount() {
     this.mounted = false;
   }
@@ -47,26 +59,27 @@ class App extends Component {
     });
   };
 
- 
-
-
-
-
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
+
     return (
       <div className="App">
-        
         <h1>Event App</h1>
-        {!navigator.onLine && <WarningAlert infoText="The app is offline, events may not be up to date." />}
-<div className="search-bar">
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents}
-        />
-        <br />
-        <NumberOfEvents updateEvents={this.updateEvents} />
+        {!navigator.onLine && (
+          <WarningAlert infoText="The app is offline, events may not be up to date." />
+        )}
+        <div className="search-bar">
+          <CitySearch
+            locations={this.state.locations}
+            updateEvents={this.updateEvents}
+          />
+          <br />
+          <NumberOfEvents updateEvents={this.updateEvents} />
         </div>
         <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
